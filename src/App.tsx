1295cf461198324692700249b5e2d674b6157a0e
Adithya P <prasadadithya714@gmail.com>
2025-05-21 15:33:49 +0530

    handled routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Login from "./component/login/Login";
+import Home from "./component/home/Home";
+import { isAuthenticated } from "./utils/auth";
+
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/home"
+          element={
+            isAuthenticated() ? <Home /> : <Navigate to="/log" replace />
+          }
+        />
+        <Route
+          path="/*"
+          element={
+            isAuthenticated() ? <Home /> : <Navigate to="/login" replace />
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
