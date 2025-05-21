4145e2d2aad17a91051b0300aa86515bdf6882f9
Adithya P <prasadadithya714@gmail.com>
2025-05-21 17:18:09 +0530

    enhanched login page and fixed routing

--- src/theme/Theme.ts
+++ src/theme/Theme.ts
@@ -6,6 +6,22 @@ const theme = createTheme({
       default: "#2D1B49",
     },
   },
+  typography: {
+    fontFamily: ["GT Walsheim", "Arial", "sans-serif"].join(","),
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          backgroundColor: "#0C7181",
+          color: "#fff",
+          "&:hover": {
+            backgroundColor: "#084f5a",
+          },
+        },
+      },
+    },
+  },
 });
 
 export default theme;
